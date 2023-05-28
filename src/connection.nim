2aa8bfadbf7b3a260e80bb8c18ff28188bd81665
RadKesvat <>
2023-05-29 03:43:29 +0430

    remove unused imports

--- src/connection.nim
+++ src/connection.nim
@@ -1,38 +1,38 @@
 import overrides/[asyncnet]
-import std/[tables,times,random,parseutils, asyncdispatch, strformat,strutils, net, random,bitops]
-import globals,pipe
+import std/[tables, times, random, asyncdispatch, strutils, net, random]
+import globals
 
 
-var lgid:uint32 = 1
-proc new_uid:uint32 = 
-    result =  lgid
+var lgid: uint32 = 1
+proc new_uid: uint32 =
+    result = lgid
     inc lgid
 
 
 type
-    TrustStatus*{.pure.} = enum 
-        no,pending,yes
+    TrustStatus*{.pure.} = enum
+        no, pending, yes
 
     Connection* = ref object
-        recv_buffer*:string
-        id*:uint32
-        creation_time*:uint
-        trusted*:TrustStatus
-        address*:string        
-        socket*:AsyncSocket
-        counter*:uint32
-            
+        recv_buffer*: string
+        id*: uint32
+        creation_time*: uint
+        trusted*: TrustStatus
+        address*: string
+        socket*: AsyncSocket
+        counter*: uint32
+
     Connections* = object
         connections*: Table[uint32, Connection]
 
 # proc has(cons:Connections,con:Connection):bool =cons.connections.hasKey(con.id)
 
-template send*(con:Connection,data:string):untyped  = con.socket.send(data)
-template recv*(con:Connection,data:SomeInteger):untyped = con.socket.recv(data)
+template send*(con: Connection, data: string): untyped = con.socket.send(data)
+template recv*(con: Connection, data: SomeInteger): untyped = con.socket.recv(data)
 
 
-proc sendF*(con:Connection,data:string){.async.}=
-    await send(con,data)
+proc sendF*(con: Connection, data: string){.async.} =
+    await send(con, data)
     # var dlen = data.len()
     # var index = 0
     # while dlen > 1024:
@@ -46,17 +46,17 @@ proc sendF*(con:Connection,data:string){.async.}=
 
 
 
-proc isClosed*(con:Connection):bool = con.socket.isClosed()
-template close*(con:Connection) = con.socket.close()
+proc isClosed*(con: Connection): bool = con.socket.isClosed()
+template close*(con: Connection) = con.socket.close()
 
-proc close*(cons:var Connections,con: Connection) =
+proc close*(cons: var Connections, con: Connection) =
     con.socket.close()
     if cons.connections.hasKey(con.id):
         cons.connections.del(con.id)
 
-proc isTrusted*(con:Connection):bool= con.trusted == TrustStatus.yes
+proc isTrusted*(con: Connection): bool = con.trusted == TrustStatus.yes
 
-proc takeRandom*(cons:Connections):Connection = 
+proc takeRandom*(cons: Connections): Connection =
     var chosen = rand(cons.connections.len()-1)
     for k in cons.connections.keys:
         if chosen == 0:
@@ -68,7 +68,7 @@ proc takeRandom*(cons:Connections):Connection =
 
 
 
-proc newConnection*(socket : AsyncSocket = nil ,address:string, buffered:bool =  globals.socket_buffered):Connection=
+proc newConnection*(socket: AsyncSocket = nil, address: string, buffered: bool = globals.socket_buffered): Connection =
     new(result)
     # result.recv_buffer =  newStringOfCap(globals.connection_buf_cap)
     # if id == 0 : result.id = new_uid()
@@ -77,12 +77,12 @@ proc newConnection*(socket : AsyncSocket = nil ,address:string, buffered:bool = 
     result.trusted = TrustStatus.pending
     result.address = address
 
-    if socket == nil: result.socket =  newAsyncSocket(buffered = buffered)
-    else:result.socket = socket
-    
+    if socket == nil: result.socket = newAsyncSocket(buffered = buffered)
+    else: result.socket = socket
+
     result.socket.setSockOpt(OptNoDelay, true)
 
-proc register*(cons: var Connections, con: Connection)=
+proc register*(cons: var Connections, con: Connection) =
     if con.id == 0:
         con.id = new_uid()
     assert not cons.connections.hasKey con.id
