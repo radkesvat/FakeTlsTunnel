25ed35b3fd041498041b4bb183e183c060d1f920
RadKesvat <>
2023-05-30 17:42:46 +0430

    increase proccess hard and soft limit

--- src/main.nim
+++ src/main.nim
@@ -1,16 +1,18 @@
 
-import std/[random,os,asyncdispatch]
+import std/[random,os,osproc,asyncdispatch]
 from globals import nil
 import tunnel,server
 
 
 when defined(linux):
+    import std/posix
     if not isAdmin():
         echo "Please run as root."
         quit(-1)
     assert 0 == execShellCmd("sudo ufw disable")
     assert 0 == execShellCmd("sysctl -w fs.file-max=100000")
-    assert 0 == execShellCmd("ulimit -n 65000")
+    var limit = RLimit(rlim_cur:65000,rlim_max:66000)
+    assert 0 == setrlimit(RLIMIT_NOFILE,limit)
 
 randomize()
 globals.init()
