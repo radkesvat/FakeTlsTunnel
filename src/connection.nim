ac46e50baafd154a1103944d7a2ca369620fc3a4
RadKesvat <>
2023-06-11 13:25:58 +0430

    implemented a connection recv idle time detector to close the dead connection

--- src/connection.nim
+++ src/connection.nim
@@ -1,5 +1,5 @@
 import overrides/[asyncnet]
-import std/[tables, times,os , random, asyncdispatch, strutils, net, random]
+import std/[tables,sequtils, times,os , random, asyncdispatch, strutils, net, random]
 import globals
 
 
@@ -7,16 +7,16 @@ var lgid: uint32 = 1
 proc new_uid: uint32 =
     result = lgid
     inc lgid
-
+var et:uint = 0
 
 type
     TrustStatus*{.pure.} = enum
         no, pending, yes
 
     Connection* = ref object
-        recv_buffer*: string
-        id*: uint32
         creation_time*: uint
+        recv_start_time*: uint
+        id*: uint32
         trusted*: TrustStatus
         address*: string
         socket*: AsyncSocket
@@ -25,13 +25,19 @@ type
     Connections* = object
         connections*: Table[uint32, Connection]
 
+var allConnections:seq[Connection]
+
 # proc has(cons:Connections,con:Connection):bool =cons.connections.hasKey(con.id)
 
-template send*(con: Connection, data: string): untyped = con.socket.send(data)
-template recv*(con: Connection, data: SomeInteger): untyped = con.socket.recv(data)
-
-
-
+template send*(con: Connection, data: string): untyped = 
+    con.socket.send(data)
+template recv*(con: Connection, data: SomeInteger): untyped = 
+    con.recv_start_time = et
+    var result = con.socket.recv(data)
+    result.addCallback(proc()=
+        con.recv_start_time = 0
+    )
+    result
 
 proc isClosed*(con: Connection): bool = con.socket.isClosed()
 template close*(con: Connection) = con.socket.close()
@@ -63,13 +69,14 @@ proc newConnection*(socket: AsyncSocket = nil, address: string, buffered: bool =
     result.creation_time = epochTime().uint32
     result.trusted = TrustStatus.pending
     result.address = address
+    result.recv_start_time = 0
 
     if socket == nil: result.socket = newAsyncSocket(buffered = buffered)
     else: result.socket = socket
 
     when not defined(android):
         result.socket.setSockOpt(OptNoDelay, true)
-
+    allConnections.add result
 
 # proc attachID*(con : var Connection)=
 #     if con.id == 0:
@@ -88,3 +95,19 @@ proc register*(cons: var Connections, con: Connection) =
 
     cons.connections[con.id] = con
 
+
+
+
+proc startController*(){.async.}=
+    while true:
+        et = epochTime().uint
+        await sleepAsync(1000)
+        var to_del:seq[int]
+        allConnections.keepIf(
+            proc(x: Connection):bool =
+                if x.recv_start_time == 0: return true
+                if et - x.recv_start_time > globals.max_idle_time :
+                    x.socket.close()
+                    return false
+                return true
+        )
