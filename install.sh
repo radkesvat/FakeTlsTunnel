b93df478121aba0165e217bb9e263a968bcaa59f
RadKesvat <>
2023-06-11 13:39:26 +0430

    v10

--- install.sh
+++ install.sh
@@ -33,10 +33,10 @@ printf  "\n"
 
 
 
-wget "https://github.com/radkesvat/FakeTlsTunnel/releases/download/V9/v9_linux_amd64.zip" -O v9_linux_amd64.zip
-unzip -o v9_linux_amd64.zip
+wget "https://github.com/radkesvat/FakeTlsTunnel/releases/download/V10/v10_linux_amd64.zip" -O v10_linux_amd64.zip
+unzip -o v10_linux_amd64.zip
 chmod +x FTT
-rm v9_linux_amd64.zip
+rm v10_linux_amd64.zip
 
 echo "finished."
 
