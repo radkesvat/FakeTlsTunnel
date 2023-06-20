8fca5676b15dd0ddc2ea4c02c576ee57ddd8a6a7
RadKesvat <>
2023-06-20 23:56:48 +0430

    version 11

--- src/globals.nim
+++ src/globals.nim
@@ -1,7 +1,7 @@
 import dns_resolve, hashes, print, parseopt, strutils, random, net, strutils, osproc , strformat
 import std/sha1
 
-const version = "10.2"
+const version = "11"
 
 type RunMode*{.pure.} = enum
     tunnel, server
