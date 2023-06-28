1b1cdd07b43bc87c13f097e3adbd4abfe6f90ca3
RadKesvat <>
2023-06-28 18:06:13 +0430

    to install 11.1 if the latest version did not work properly

---
+++ install_11.1.sh
@@ -0,0 +1,43 @@
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+# echo nameserver 8.8.8.8 | sudo tee /etc/resolv.conf
+
+apt-get update -y
+
+REQUIRED_PKG="unzip"
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
+echo Checking for $REQUIRED_PKG: $PKG_OK
+if [ "" = "$PKG_OK" ]; then
+  echo "Setting up $REQUIRED_PKG."
+  sudo apt-get --yes install $REQUIRED_PKG
+fi
+
+REQUIRED_PKG="wget"
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
+echo Checking for $REQUIRED_PKG: $PKG_OK
+if [ "" = "$PKG_OK" ]; then
+  echo "Setting up $REQUIRED_PKG."
+  sudo apt-get --yes install $REQUIRED_PKG
+fi
+
+pkill FTT
+printf  "\n"
+printf  "\n"
+
+
+echo "downloading FakeTlsTunnel"
+
+printf  "\n"
+
+
+
+wget "https://github.com/radkesvat/FakeTlsTunnel/releases/download/V11.1/v11.1_linux_amd64.zip" -O v11.1_linux_amd64.zip
+unzip -o v11.1_linux_amd64.zip
+chmod +x FTT
+rm v11.1_linux_amd64.zip
+
+echo "finished."
+
+printf  "\n"
