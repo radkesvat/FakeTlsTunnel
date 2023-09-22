4b58c95b8a681a82f88815596453bb7c0408954c
Peyman <118217228+Ptechgithub@users.noreply.github.com>
2023-09-22 17:05:39 +0330

    update FtTunnel.sh

--- FtTunnel.sh
+++ FtTunnel.sh
@@ -73,7 +73,7 @@ configure_arguments2() {
         read -p "Please Enter Password (Please choose the same password on both servers): " password
         arguments="--server --lport:443 --toip:127.0.0.1 --toport:$port --sni:$sni --password:$password --terminate:24"
     elif [ "$server_choice" == "1" ]; then
-        read -p "Please Enter (Kharej IP) : " server_ip
+        read -p "Please Enter (Kharej IP(extrenal-server)) : " server_ip
         read -p "Please Enter Password (Please choose the same password on both servers): " password
         arguments="--tunnel --lport:$port --toip:$server_ip  --toport:443 --sni:$sni --password:$password --terminate:24"
     else
@@ -93,7 +93,7 @@ configure_arguments() {
         read -p "Please Enter Password (Please choose the same password on both servers): " password
         arguments="--server --lport:443 --toip:127.0.0.1 --toport:2000 --password:$password --multiport --sni:$sni --terminate:24"
     elif [ "$server_choice" == "1" ]; then
-        read -p "Please Enter (Kharej IP) : " server_ip
+        read -p "Please Enter (Kharej IP(extrenal-server)) : " server_ip
         read -p "Please Enter Password (Please choose the same password on both servers): " password
         arguments="--tunnel --lport:23-65535 --toip:$server_ip --toport:443 --password:$password --sni:$sni --terminate:24"
     else
